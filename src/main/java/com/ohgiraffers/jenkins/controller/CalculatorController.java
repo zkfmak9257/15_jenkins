73d4aa6838af4881215f8122781c71cc62ae53fd
zkfmak9257 <zkfmak9257@naver.com>
2026-02-20 11:24:51 +0900

    jenkins pipeline test

--- src/main/java/com/ohgiraffers/jenkins/controller/CalculatorController.java
+++ src/main/java/com/ohgiraffers/jenkins/controller/CalculatorController.java
@@ -18,7 +18,7 @@ public class CalculatorController {
 
     @GetMapping("/health")
     public String healthCheck() {
-        return "health check";
+        return "jenkins pipe line success";
     }
 
     @GetMapping("/plus")
